9ed8ce2befcc40c83b0f7e78dfd5ee9cad9b0970
suzuki-yusuke <31797301+suzuki-yusuke@users.noreply.github.com>
2022-11-28 15:36:21 +0900

    Update __init__.py

--- caiman/source_extraction/cnmf/__init__.py
+++ caiman/source_extraction/cnmf/__init__.py
@@ -9,7 +9,7 @@ from . import merging
 from . import pre_processing
 from . import spatial
 from . import temporal
-from . import oasis
+#from . import oasis
 from . import params
 from . import online_cnmf
 from .cnmf import CNMF as CNMF
